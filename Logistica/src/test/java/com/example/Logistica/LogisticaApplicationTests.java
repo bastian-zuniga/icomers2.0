847aa666b2b8e063cf94bf8fea5f5446c8e87c8e
MatiasC <mat.contrerasg@duocuc.cl>
2026-06-26 20:55:36 -0400

    Nuevas Categorias Microservicios

---
+++ Logistica/src/test/java/com/example/Logistica/LogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
